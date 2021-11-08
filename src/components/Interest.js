11015646ed27b5ec910f72f4cedeb0ff45be554b
wantris <gustiwacik@gmail.com>
2021-11-08 09:56:52 +0700

    fix margin in project container

--- src/components/Interest.js
+++ src/components/Interest.js
@@ -1,75 +1,75 @@
 import React, { Component } from 'react'
 import ProjectDataService from '../services/project.service'
 
-export default class Interest extends Component{
+export default class Interest extends Component {
     constructor(props) {
         super(props);
         this.retrieveProject = this.retrieveProject.bind(this);
-    
+
         this.state = {
-          project: [],
+            project: [],
         };
     }
 
     componentDidMount() {
         this.retrieveProject();
-      }
+    }
 
-    retrieveProject(){
+    retrieveProject() {
         ProjectDataService.get()
             .then(response => {
                 this.setState({
                     project: response.data.data
                 });
             })
-        .catch(e => {
-            console.log(e);
-        });
+            .catch(e => {
+                console.log(e);
+            });
     }
 
-    render(){
-        const {project} = this.state;
-        
-        
-        function previewImage(e){
+    render() {
+        const { project } = this.state;
+
+
+        function previewImage(e) {
             const id = e.target.id;
             var modal = document.getElementById("myModal");
-            var img = document.getElementById("img-project_"+id);
+            var img = document.getElementById("img-project_" + id);
             var modalImg = document.getElementById("img01");
 
             // console.log(modalImg);
-    
+
             modal.style.display = "block";
             modalImg.src = e.target.src;
-    
+
         }
-    
-        function closePreview (e) {
+
+        function closePreview(e) {
             var modal = document.getElementById("myModal");
             modal.style.display = "none";
         }
-        
 
-        return(
-            <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+
+        return (
+            <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="projects">
                 <div className="my-auto">
                     <h2 className="mb-5">Project</h2>
 
                     {project.map((item, index) => (
-                            <div className="row">
-                                <div className="col-lg-8 col-12">
-                                    <h3 className="mb-0">{item.title}</h3>
-                                    <p>{item.description}</p>
-                                </div>
-                                <div className="col-lg-4 col-12">
-                                    <span className="text-primary">{item.date_start} - {item.date_end}</span>
-                                </div>
-                                <div className="col-lg-12 col-12 text-center">
-                                    <img className="img-fluid img-project mt-3" id={"img-project_"+item.id} alt={item.title} src={item.photo} onClick={previewImage}></img>
-                                </div>
+                        <div className="row mb-4">
+                            <div className="col-lg-8 col-12">
+                                <h3 className="mb-0">{item.title}</h3>
+                                <p>{item.description}</p>
                             </div>
-                            )
-                        )
+                            <div className="col-lg-4 col-12">
+                                <span className="text-primary">{item.date_start} - {item.date_end}</span>
+                            </div>
+                            <div className="col-lg-12 col-12 text-center">
+                                <img className="img-fluid img-project mt-3" id={"img-project_" + item.id} alt={item.title} src={item.photo} onClick={previewImage}></img>
+                            </div>
+                        </div>
+                    )
+                    )
 
                     }
 
@@ -81,7 +81,7 @@ export default class Interest extends Component{
                     <div id="caption-project"></div>
                 </div>
             </section>
-            
+
         )
     }
 }
